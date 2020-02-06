d444b4130085f3926f6538b0f37c23805fa3d62e
rahmandihamzah <rahmandi.dev@gmail.com>
2020-02-06 23:39:03 +0700

    signin-signout

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,53 @@
 // show hide element
+let token = localStorage.getItem('token')
+let gToken = localStorage.getItem('gToken')
+
+function welcomePage() {
+    $('#signIn-container').hide()
+    $('#welcome').show()
+    $('#nav-signIn').show()
+    $('#nav-signOut').hide()
+    $('#nav-gSignOut').hide()
+    // $('#nav-gSignOut').show()
+}
+
+$(document).ready(() => { 
+    if (token) {
+        $('#logo').on('click', () => {
+            welcomePage()
+        })
+        $('#signIn-container').hide()
+        $('#nav-signIn').hide()
+
+        if (gToken) {
+            $('#nav-signOut').hide()
+            $('#nav-gSignOut').show()
+        } else {
+            $('#nav-signOut').show()
+            $('#nav-gSignOut').hide()
+        }
+    } else {
+        welcomePage()
+        $('#logo').on('click', () => {
+            welcomePage()
+            $('#nav-signIn').show()
+            $('#nav-signOut').hide()
+            $('#nav-gSignOut').hide()
+        })
+    }
+
+    $('#nav-signIn').on('click', () => {
+        $('#signIn-container').show()
+        $('#welcome').show()
+    })
+
+    $('#signIn-form').on('submit', (event) => {
+        event.preventDefault()
+        login()
+    })
+
+    $('#nav-signOut').on('click', () => {
+        logout()
+        // $('#welcome').show()
+    })
+})
