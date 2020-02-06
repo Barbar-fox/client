5ed528c41e2bdd0e1902642bb048991a801cdb78
Fikrimarzuki <fikri.marzuki1993@gmail.com>
2020-02-06 20:56:21 +0700

    register html main auth done

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,18 @@
 // show hide element
+
+
+
+$(document).ready(function() {
+   //function on click register
+   $("nav#navRegister").on("click", function(el) {
+      el.preventDefault()
+      $("div#registerForm").show()
+      $("#submitRegister").on("click", function(e) {
+         e.preventDefault()
+         register()
+      })
+   })
+
+
+   
+})
